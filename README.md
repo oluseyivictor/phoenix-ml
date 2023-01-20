24dedf019010f2ea7afe8981ae1e6f8c9afe7a15
oluseyivictor <oloniyov@gmail.com>
2023-01-20 09:53:36 +0000

    restructuring files and added transparent versioning

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # gherkin-ml
+
+Gherkin ml is a lightweight and easy to use machine learning framework for use in simple applications
--- app/main.cpp
+++ app/main.cpp
@@ -8,6 +8,7 @@ using namespace gherkin;
 int main(){
 
     std::cout<< project_name <<std::endl;
+    std::cout<< project_version <<std::endl;
 
 double ment2[3] = {0.2,0.4,0.6};
 
