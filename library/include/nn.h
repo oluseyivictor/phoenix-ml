c608d5a0441629c7f4779a87467f2cdbb9526693
oluseyivictor <oloniyov@gmail.com>
2023-02-21 10:38:20 +0000

    Introduced configurable activation funtions for all layers

--- library/include/nn.h
+++ library/include/nn.h
@@ -9,6 +9,8 @@
 #include "Matrix.h"
 #include "Vector.h"
 #include "utils.h"
+#include <functional>
+
 
 using namespace phoenix;
 
@@ -17,18 +19,22 @@ using namespace phoenix;
   template <typename T>
 class NeuralModel {
 
-public:
+private:
     Matrix<double> feature;
     Matrix<double> label;
     Tensor<T> network;
-    std::initializer_list<int> hids;
-    std::vector<Vector<T>> B;
+    std::initializer_list<int> hids;    //number of hidden neuron in each layer
+    std::vector<Vector<T>> B;           //Biases
     std::vector<Vector<T>> target;
     int no_hid = 0;
     double learning_rate = 0.01;
+    
+   struct act{
+    std::function<double(double)> activation;
+    std::function<double(double)> derivative;
+   }; 
 
-
-    //double act(double value) {return sigmoid_derivative(value);}
+   std::vector<act> A;    //activation_functions;
 
 public:
     /**
@@ -40,6 +46,7 @@ public:
                  double rate) : feature(in), label(out), hids(hidden_neurons), learning_rate(rate) 
                  {
                     no_hid = hidden_neurons.size();
+                    NNBuild();
                  };
 
     /**
@@ -47,40 +54,76 @@ public:
      */
     ~NeuralModel()  {};
 
-    //template <typename T>
-    void construct_network(const Vector<T> &input,
-                           const Vector<T> &output)
+    void NNConnfigure(std::initializer_list<std::string> act_function)
+    {
+      //Empty all elements of activation initializer
+      if (!A.empty()){
+        A.clear();
+      }
+
+
+      for (auto fn : act_function)
+      {
+        act activationType;
+
+        if (fn == "sigmoid")
+        {
+          activationType.activation = sigmoid;
+          activationType.derivative = sigmoid_derivative;
+        }
+        else if (fn == "relu") 
+        {
+          activationType.activation = relu;
+          activationType.derivative = relu_derivative;
+        }
+        else if (fn == "linear")
+        {
+          activationType.activation = linear;
+          activationType.derivative = linear_derivative;
+        }
+        else{
+          std::cout << "invalid actvation function"<<std::endl;
+        }
+
+     A.push_back(activationType); 
+
+      }
+    }
+
+private:
+
+    //Build NN network with input -- hidden layers and  -- output
+    void NNBuild()
     {
 
-        target.push_back(output);
-
-        int bk_n = input.size();
+        int bk_n = feature.getCols();
+        Vector<T> input(bk_n);
         
-
         network.addMatrix(input);
         
 
-        for (auto n: hids)     
+        for (auto n: hids)   //Add hidden layer neurons to network    
         {
             Vector<T> neurons(n);
             Vector<T> hidden_bias(n);
 
-            Matrix<T> weight(n,bk_n);
+            Matrix<T> weight(n,bk_n);   //construct weight matrices
 
             weight.randfill();
-            hidden_bias.randfill();
+            hidden_bias.zerofill();
 
             network.addMatrix(weight);
             network.addMatrix(neurons);
             B.push_back(hidden_bias);
 
-
             bk_n = neurons.size();
         }
 
-        int last_n = output.size();
+        int last_n = label.getCols();
 
-        Vector<T> output_bias(last_n);
+        Vector<T> output(last_n); 
+        Vector<T> output_bias(last_n);   //this is made redundant since bias is not added to output
+        output_bias.zerofill();
         B.push_back(output_bias);
         
 
@@ -88,55 +131,67 @@ public:
         weight.randfill();
         network.addMatrix(weight);
         network.addMatrix(output);
-        
 
        // network.print();
+       act default_fn = {sigmoid, sigmoid_derivative};
+       act output_fn = {linear, linear_derivative};
+
+       for (int i = 0; i < no_hid; i++)
+       {
+        A.push_back(default_fn);
+       }
+       A.push_back(output_fn);
+
     }
 
-    void forward_propagation(){
+    void forward_propagation(const Vector<T> &input){
 
         int layer= 0;
         int i = 0;
+        network[layer] = input;
 
         //Calculate output of hidden layer
         for ( i = 0; i < no_hid; i++)
         {
-           auto hidden_layer = convert_col(network[layer++], 0);
+           auto hidden_layer = convert_col(network[layer++], 0);  //convert input matrix to vector
         
 
           // auto p = vecTomat_multiply(hidden_layer, network[layer++]);
         auto p = (network[layer++] * hidden_layer) + B[i];
-        network[layer] = vector_sigmoid(p);
+        //network[layer] = vector_sigmoid(p);
+        network[layer] = vector_act(p, A[i].activation);
      
         }
 
         //Calculate final output  no sigmoid
         auto hidden_layer = convert_col(network[layer++], 0);
         auto p = (network[layer++] * hidden_layer);   // + B[i];
-        network[layer] = p;
+        //network[layer] = p;
+        network[layer] = vector_act(p, A[no_hid].activation);
 
-        network.print();
+      //   network.print();
+
     }
 
-
-    void back_propagation(){
+    void back_propagation(const Vector<T> &expected_output){
 
     std::vector<Vector<double>> error;
     int count = network.size() - 1;
  
     //Extract Value of output from tensor network
     Vector<double> output =  convert_col(network[count], 0);
-    Vector<double> expected_output = target[0];
+    // Vector<double> expected_output = target[0];
     
      //calculate error for the output layer
      Vector<double> output_error(output.size());
      for (int i = 0; i < output.size(); i++)
      {
         double derr = expected_output[i] - output[i];
-        output_error[i] = derr * sigmoid_derivative(output[i]);
+        output_error[i] = derr * A[no_hid].derivative(output[i]);
      }
     error.push_back(output_error);
-   std::cout<<"-----------------------------"<<std::endl;
+ 
+
 
     //calculate error for the hidden layer
     for (int hid = 0; hid < no_hid; hid++)
@@ -150,14 +205,9 @@ public:
         h_error = h_weight * error[hid];
 
         for (int i =0; i < h_error.size(); i++)
-            h_error[i] = h_error[i] * sigmoid_derivative(hidden_n[i]);
+            h_error[i] =   h_error[i] * A[hid].derivative(hidden_n[i]);
 
     error.push_back(h_error);
-
-    //  std::cout<<h_weight<<std::endl;
-    //  std::cout<<hidden_n<<std::endl;
-    //  std::cout<< error[hid]<<std::endl;
-    //   std::cout<<"-----------------------------"<<std::endl;
 
     }
 
@@ -184,11 +234,55 @@ public:
       }
     
     }
-    network.print();
-    //std::cout<<B.size()<<std::endl;
-            
+     //network.print();
 
     }   
+  
+  public:
+
+  Vector<T> NNpredict(const Vector<T> &predict)
+  {
+    int count = network.size() - 1;
+
+    forward_propagation(predict);
+
+    Vector<double> output =  convert_col(network[count], 0);
+
+    return (output);
+
+  }
+
+  Vector<T> NNPredicted()
+  {
+    int count = network.size() - 1;
+
+    Vector<double> output =  convert_col(network[count], 0);
+
+    return (output);
+  }
+
+void NNTrain(int epochs = 100)
+{
+  double error;
+
+  for (int count = 0; count < epochs; count++)
+  {
+    error = 0;
+    for(int i = 0; i < feature.rows; i++)
+    {
+      auto input = convert_row(feature, i);
+      auto target = convert_row(label, i);
+
+      forward_propagation(input);
+      back_propagation(target);
+
+      error += total_error(target,  NNPredicted());
+    }
+
+    std::cout<<"Epoch----- "<< count << " Error: "<<error/feature.rows<<std::endl;
+  }
+
+}
     
 
 };
