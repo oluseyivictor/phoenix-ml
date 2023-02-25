16eaf3db2c9f042c315114d1d01b3e8cc792ecc7
oluseyivictor <oloniyov@gmail.com>
2023-02-25 20:04:16 +0000

    Reformat read input function to allow delimeter separation

--- library/include/io.h
+++ library/include/io.h
@@ -7,23 +7,33 @@
 using namespace phoenix;
 
 
-template<typename T>
-void ReadFileToMatrix(const std::string& filename, Matrix<T>& matrix) {
+
+Matrix<double> ReadFileToMatrix(const std::string& filename, char delimeter) {
+
+  Matrix<double> matrix(1,1);
+
+
   std::ifstream file(filename);
   if (!file.is_open()) {
     std::cerr << "Error: Could not open file " << filename << std::endl;
-    return;
+    return matrix;
   }
+
+   //char delimeter = ' ';  
+
+   std::string token;
 
   std::string line;
   std::vector<double> values;
   int rows = 0, cols, col = 0;
 
+try{
   while (std::getline(file, line)) {
     std::stringstream line_stream(line);
     double value;
     col = 0;
-    while (line_stream >> value) {
+    while (std::getline (line_stream, token, delimeter)){    
+      double value = std::stod(token);                             // while (line_stream >> value) {
       if (col == 0) {
         cols++;
       }
@@ -33,14 +43,24 @@ void ReadFileToMatrix(const std::string& filename, Matrix<T>& matrix) {
     ++rows;
   }
 
- std::cout<< rows<< " "<<col <<std::endl;
-  matrix = Matrix<T>(rows, col);
+  matrix = Matrix<double>(rows, col);
   for (int i = 0; i < rows; ++i) {
     for (int j = 0; j < col; ++j) {
       matrix(i, j) = values[i * col + j];
+      
     }
   }
 
   file.close();
 }
 
+catch (const std::invalid_argument& e)
+{
+  std::cerr << "file conversion error check delimeter or file "<<e.what()<< std::endl;
+  file.close();
+  return matrix;
+}
+
+return matrix;
+
+}
