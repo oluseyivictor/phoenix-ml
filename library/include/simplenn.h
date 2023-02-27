972b34f668c6cca3e939eff5250454bbf0f22ae0
oluseyivictor <oloniyov@gmail.com>
2023-02-27 10:59:58 +0000

    Added model load function

--- library/include/simplenn.h
+++ library/include/simplenn.h
@@ -18,18 +18,20 @@ class SimpleNeuralNetwork : public NeuralModel, public INeuralNetwork {
 
         Matrix<double> input;
         Matrix<double> output;
+        std::vector<int> H;    //number of hidden neuron in each layer
 
         int num_inputs;
         int num_hidden;
         int num_outputs;
         double learning_rate;
+        int value;
         //create a new struct from predicted for load and save --- to be added
     
     public:
     SimpleNeuralNetwork (Matrix<double> &rin,
                  Matrix<double> &rout,
                  std::initializer_list<int> rhidden_neurons,
-                 double rrate): NeuralModel(rin, rout, rhidden_neurons, rrate), input(rin) , output(rout) 
+                 double rrate): NeuralModel(rin, rout, rhidden_neurons, rrate), input(rin) , output(rout)
                  {
 
 
@@ -37,7 +39,16 @@ class SimpleNeuralNetwork : public NeuralModel, public INeuralNetwork {
                     num_hidden = rhidden_neurons.size();
                     num_outputs = rout.getCols();
                     learning_rate = rrate;
+
+                    for (int n: rhidden_neurons){
+                        H.push_back(n);
+                    }
+
                  }
+         SimpleNeuralNetwork(): NeuralModel()
+         {
+
+         }
 
      void train(const int epochs = 100){
          double error;
@@ -56,9 +67,6 @@ class SimpleNeuralNetwork : public NeuralModel, public INeuralNetwork {
             error += total_error(target,  NeuralModel::NNPredicted());
           }
           error = error/input.getRows();
-        //    if ( error > 100) error = 100;
-        //    else error = 100 - error;
-
            std::cout<<"Epoch----- "<< count << " Error: "<<error<<std::endl;
          }
  }
@@ -93,29 +101,76 @@ class SimpleNeuralNetwork : public NeuralModel, public INeuralNetwork {
         file.write(reinterpret_cast<char*>(&num_hidden), sizeof(num_hidden));
         file.write(reinterpret_cast<char*>(&num_outputs), sizeof(num_outputs));
         file.write(reinterpret_cast<char*>(&learning_rate), sizeof(learning_rate));
+
         
-        int bias_size = NeuralModel::B.size();
-        for (int i = 0; i < bias_size; i++ )
-        {
-            Matrix<double> bias = NeuralModel::B[i];
-            std::cout<<bias<<std::endl;
-            file.write(reinterpret_cast<char*>(bias.getdata()), bias.size() * sizeof(double)) ;       
-
+        for (auto vals: H){
+            file.write(reinterpret_cast<char*>(&vals), sizeof(vals));
         }
-
+        
         int network_size = NeuralModel::network.size() - 1;
         for (int i = 1; i < network_size; i = i+2 )
         {
             Matrix<double> weight = NeuralModel::network[i];
-            std::cout<<weight<<std::endl;
             file.write(reinterpret_cast<char*>(weight.getdata()), weight.size() * sizeof(double)) ;       
 
         }
 
+                
+        
+        int bias_size = NeuralModel::B.size();
+        for (int i = 0; i < bias_size; i++ )
+        {
+            Vector<double> bias = NeuralModel::B[i];
+            file.write(reinterpret_cast<char*>(bias.getdata()), bias.size() * sizeof(double)) ;       
+
+        }
         file.close();
     }
 
+    void load(std::string filename) {
+        std::ifstream file(filename, std::ios::in | std::ios::binary);
+        if (!file) {
+            std::cerr << "Error: Could not open file " << filename << " for reading\n";
+            return;
+        }
+        file.read(reinterpret_cast<char*>(&num_inputs), sizeof(num_inputs));
+        file.read(reinterpret_cast<char*>(&num_hidden), sizeof(num_hidden));
+        file.read(reinterpret_cast<char*>(&num_outputs), sizeof(num_outputs));
+        file.read(reinterpret_cast<char*>(&learning_rate), sizeof(learning_rate));
+
+
+        std::vector<int> nl;
+
+        for (int i = 0; i < num_hidden; i++ )
+        {
+            file.read(reinterpret_cast<char*>(&value), sizeof(value));
+            nl.push_back(value);   
+
+        }
+
+        NNBuild(num_inputs, num_outputs, nl);
+
+        int network_size = NeuralModel::network.size() - 1;
+        for (int i = 1; i < network_size; i = i+2 )
+        {
+            Matrix<double> &weight =  NeuralModel::network[i];
+           file.read(reinterpret_cast<char*>(weight.getdata()), weight.size() * sizeof(double));   
+
+        }
+
+        B.clear();
+        for (int i = 0; i < num_hidden; i++ )
+        {
+            Vector<double> bias(nl[i]);
+            file.read(reinterpret_cast<char*>(bias.getdata()), bias.size() * sizeof(double));  
+            B.push_back(bias);
+
+        }
+        file.close();
+    }
 };
+
+
 
 
 #endif
