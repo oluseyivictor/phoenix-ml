9040ff2e7f980de0367cd3e0986f61984dcb7885
oluseyivictor <oloniyov@gmail.com>
2023-02-27 11:02:05 +0000

    added load interface, removed print interface

--- library/include/nn_interface.h
+++ library/include/nn_interface.h
@@ -11,7 +11,5 @@ public:
     virtual Vector<double> predict(const Vector<double> &predict) = 0;
     virtual void save(std::string filename) = 0;
     virtual void configure(std::initializer_list<std::string> &act_value) = 0;
-    //virtual void load(std::string filename) = 0;
-    //virtual void print(void) = 0;
-    
+    virtual void load(std::string filename) = 0;
 };
